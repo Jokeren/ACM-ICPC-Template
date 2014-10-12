daf162c6857227e01e0db81af161515086f03ccb
KerenZhou <robinho364@gmail.com>
2014-10-12 09:00:47 +0800

    Rectification

---
+++ src/str_suffix_array_function.h
@@ -0,0 +1,175 @@
+#define _USE_MATH_DEFINES
+
+#ifdef ONLINE_JUDGE
+#define FINPUT(file) 0
+#define FOUTPUT(file) 0
+#else
+#define FINPUT(file) freopen(file,"r",stdin)
+#define FOUTPUT(file) freopen(file,"w",stdout)
+#endif
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <set>
+#include <stack>
+#include <string>
+#include <map>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <functional>
+
+typedef long long ll;
+static const int N = 100010;
+static const int M = 128;
+static const int LEN = 1010;//>=128
+static const int MAX = 0x7fffffff;
+static const int GMAX = 0x3f3f3f3f;
+static const int MIN = ~MAX;
+static const double EPS = 1e-7;
+static const int BASE = 10007;
+static const int CH = 200;
+
+char word[LEN];
+int s[LEN], sa[LEN], t[LEN], t2[LEN], c[LEN];
+int rank[LEN], height[LEN];
+
+void build_sa(int m, int n)
+{
+	int i, *x = t, *y = t2;
+
+	for (i = 0; i < m; i++)
+		c[i] = 0;
+
+	for (i = 0; i < n; i++)
+		c[x[i] = s[i]]++;
+	for (i = 1; i < m; i++)
+		c[i] += c[i - 1];
+	for (i = n - 1; i >= 0; i--)
+		sa[--c[x[i]]] = i;
+
+	for (int k = 1; k <= n; k <<= 1) {
+
+		int p = 0;
+		for (i = n - k; i < n; i++)
+			y[p++] = i;
+
+		for (i = 0; i < n; i++) {
+			if (sa[i] >= k) {
+				y[p++] = sa[i] - k;
+			}
+		}
+
+		for (i = 0; i < m; i++)
+			c[i] = 0;
+
+		for (i = 0; i < n; i++)
+			c[x[y[i]]]++;
+
+		for (i = 0; i < m; i++)
+			c[i] += c[i - 1];
+
+		for (i = n - 1; i >= 0; i--)
+			sa[--c[x[y[i]]]] = y[i];
+
+		std::swap(x, y);
+		p = 1;
+		x[sa[0]] = 0;
+
+		for (i = 1; i < n; i++)
+			x[sa[i]] = y[sa[i - 1]] == y[sa[i]] && y[sa[i - 1] + k] == y[sa[i] + k] ? p - 1 : p++;
+
+		if (p >= n)
+			break;
+		m = p;
+	}
+}
+
+int inline cmp_suffix(char* pattern, int pattern_len, int p)
+{
+	return strncmp(pattern, s + sa[p], pattern_len);
+}
+
+int find(char* pattern, int pattern_len, int temp_len)
+{
+	if (cmp_suffix(pattern, pattern_len, 0) < 0)
+		return -1;
+
+	if (cmp_suffix(pattern, pattern_len, temp_len - 1) > 0)
+		return -1;
+
+	int l = 0, r = temp_len - 1;
+
+	while (r >= l) {
+		int m = l + (r - l) / 2;
+
+		int res = cmp_suffix(pattern, pattern_len, m);
+
+		if (!res)
+			return m;
+
+		if (res < 0) {
+			r = m - 1;
+		}
+		else {
+			l = m + 1;
+		}
+	}
+
+	return -1;
+}
+
+void get_height(int n)
+{
+	int i, j, k = 0;
+
+	for (i = 1; i <= n; i++)
+		rank[sa[i]] = i;
+
+	for (i = 0; i < n; height[rank[i++]] = k) {
+		if (k) k--;
+
+		int j = sa[rank[i] - 1];
+		while (s[i + k] == s[j + k])
+			k++;
+	}
+}
+
+void solve(int n)
+{
+	build_sa(CH, n + 1);//n和n + 1搞错了，为什么？
+
+	get_height(n);
+
+    int ans = 0;
+	for (int i = 1; i <= n; i++) {
+        //printf("i%d sa[i] %d\n", i, sa[i]);
+        ans += (n - sa[i] - height[i]);
+	}
+
+	printf("%d\n", ans);
+}
+
+int main()
+{
+    FINPUT("in.txt");
+    FOUTPUT("out.txt");
+
+	int t;
+	while (scanf("%d", &t) != EOF) {
+		for (int i = 0; i < t; i++) {
+			scanf("%s", word);
+			int len = strlen(word);
+			for (int j = 0; j < len; j++) {
+                s[j] = word[j] + 1;
+			}
+			s[len] = 0;
+			solve(len);
+		}
+	}
+	return 0;
+}
--- src/str_trie_graph_class.h
+++ src/str_trie_graph_class.h
@@ -1,11 +1,47 @@
+#define _USE_MATH_DEFINES
+
+#ifdef ONLINE_JUDGE
+#define FINPUT(file) 0
+#define FOUTPUT(file) 0
+#else
+#define FINPUT(file) freopen(file,"r",stdin)
+#define FOUTPUT(file) freopen(file,"w",stdout)
+#endif
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <set>
+#include <stack>
+#include <string>
+#include <map>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <functional>
+
+typedef long long ll;
+static const int N = 500 * 210;
+static const int M = 80;
+static const int LEN = 1010;//>=128
+static const int MAX = 0x7fffffff;
+static const int GMAX = 0x3f3f3f3f;
+static const int MIN = ~MAX;
+static const double EPS = 1e-7;
+static const int BASE = 10007;
+static const int CH = 27;//å­—ç¬¦æ”¹å˜æ—¶ä¿®æ”¹
+
 class TrieGraph
 {
 private:
-	int trie[N][CH]; //NÊÇ½ÚµãÊıÄ¿£¬×Ó´®Êı*³¤¶È
+	int trie[N][CH]; //Næ˜¯èŠ‚ç‚¹æ•°ç›®ï¼Œå­ä¸²æ•°*é•¿åº¦
 	int fail[N];
 	int num[N];
 	int cur_idx;
-	char str_main[LEN]; //LENÊÇÖ÷´®³¤¶È
+	char str_main[LEN]; //LENæ˜¯ä¸»ä¸²é•¿åº¦
 
 public:
 	TrieGraph() :cur_idx(1) {
@@ -14,7 +50,7 @@ public:
 		memset(num, 0, sizeof(num));
 
 	}
-	
+
 	void init() {
 		memset(trie[0], 0, sizeof(trie[0]));
 		memset(fail, 0, sizeof(fail));
@@ -22,15 +58,15 @@ public:
 		cur_idx = 1;
 		num[0] = 0;
 	}
-	
+
 	void insert(char* s) {
-		int i = 0, p = 0;//p±íÊ¾²ãÊı
+		int i = 0, p = 0;//pè¡¨ç¤ºå±‚æ•°
 		int len = strlen(s);
 
 		for (i = 0; i <= len - 1; i++)
 		{
-			int c = s[i] - 'a' + 1;//×Ö·û¸Ä±äÊ±ĞŞ¸Ä
-			if (!trie[p][c]){//¶à´®¹¹Ôì£¬Èç¹ûÖ»ÓĞÒ»¸ö´®£¬Õâ¸öÅĞ¶Ïµ±È»Ã»ÓĞÓÃ
+			int c = s[i] - 'a' + 1;//å­—ç¬¦æ”¹å˜æ—¶ä¿®æ”¹
+			if (!trie[p][c]){//å¤šä¸²æ„é€ ï¼Œå¦‚æœåªæœ‰ä¸€ä¸ªä¸²ï¼Œè¿™ä¸ªåˆ¤æ–­å½“ç„¶æ²¡æœ‰ç”¨
 				memset(trie[cur_idx], 0, sizeof(trie[cur_idx]));
 				num[cur_idx] = 0;
 				trie[p][c] = cur_idx++;
@@ -39,7 +75,7 @@ public:
 		}
 		num[p]++;
 	}
-	
+
 	void build() {
 		std::queue<int> q;
 		for (int i = 1; i <= CH - 1; i++) {
@@ -54,18 +90,18 @@ public:
 			int p = q.front();
 			q.pop();
 
-			//failËµÃ÷
-			//¿ÉÄÜÔÚ¸¸½ÚµãµÄºó×ºÖĞÕÒµ½¶ÔÓ¦×Ö·û£¨¸¸½Úµãºó×º±¾ÉíÈ±Ê§×îºóÒ»¸ö×Ö·û£©¡£
-			//¿ÉÄÜÃ»ÓĞÕÒµ½¶ÔÓ¦×Ö·û¡£¼ÈÈ»ÔÚÕâÀïÕÒ²»µ½ºó×ºÁË£¬ËùÒÔ¾Í´ú±í¿Ï¶¨²»Æ¥ÅäÁË£¬ÄÇÃ´¿ÉÒÔ¼ÌĞø»ØËİ¡£
+			//failè¯´æ˜
+			//å¯èƒ½åœ¨çˆ¶èŠ‚ç‚¹çš„åç¼€ä¸­æ‰¾åˆ°å¯¹åº”å­—ç¬¦ï¼ˆçˆ¶èŠ‚ç‚¹åç¼€æœ¬èº«ç¼ºå¤±æœ€åä¸€ä¸ªå­—ç¬¦ï¼‰ã€‚
+			//å¯èƒ½æ²¡æœ‰æ‰¾åˆ°å¯¹åº”å­—ç¬¦ã€‚æ—¢ç„¶åœ¨è¿™é‡Œæ‰¾ä¸åˆ°åç¼€äº†ï¼Œæ‰€ä»¥å°±ä»£è¡¨è‚¯å®šä¸åŒ¹é…äº†ï¼Œé‚£ä¹ˆå¯ä»¥ç»§ç»­å›æº¯ã€‚
 			for (int i = 1; i <= CH - 1; i++)
 			{
-				//µÚ1ÖÖÇé¿ö£¬ÕÒµ½×Ö·û£¬ÉèÖÃfail£¬¼ÌĞøÇ°½ø¡£
+				//ç¬¬1ç§æƒ…å†µï¼Œæ‰¾åˆ°å­—ç¬¦ï¼Œè®¾ç½®failï¼Œç»§ç»­å‰è¿›ã€‚
 				if (trie[p][i])
 				{
 					int v = trie[p][i];
 					q.push(v);
 					fail[v] = trie[fail[p]][i];
-				}//µÚ2ÖÖÇé¿ö£¬ÒòÎªÃ»ÓĞÕÒµ½×Ö·û£¬ËùÒÔÖ±½Ó»ØËİ
+				}//ç¬¬2ç§æƒ…å†µï¼Œå› ä¸ºæ²¡æœ‰æ‰¾åˆ°å­—ç¬¦ï¼Œæ‰€ä»¥ç›´æ¥å›æº¯
 				else
 				{
 					trie[p][i] = trie[fail[p]][i];
@@ -74,25 +110,46 @@ public:
 		}
 	}
 
-	bool find() {
+	bool find(int idx, int& total, int n) {
 		int ans = 0, i = 0, p = 0;
+		if (!is_build) {
+            is_build = true;
+            build();//ç«Ÿç„¶å¿˜è®°è°ƒç”¨buildäº†...
+		}
 
 		int len = strlen(str_main);
+		bool flag = false;
+
+        bool virus[501];
+        memset(virus, false, sizeof(virus));
 		for (i = 0; i <= len - 1; i++)
 		{
-			int c = str_main[i] - 'a' + 1;
+			int c = str_main[i] + 1;//å­—ç¬¦æ”¹å˜æ—¶ä¿®æ”¹
+
+			while (p && !trie[p][c]) p = fail[p];
+
 			p = trie[p][c];
-			int tmp = p;
-			while (tmp && num[tmp] != -1)
-			{
-				if (num[tmp] > 0) {
-					return true;
-				}
-				ans += num[tmp];
-				num[tmp] = -1;
-				tmp = fail[tmp];
+
+			if (p && num[p] > 0) {
+                if (!flag) {
+                    flag = true;
+                    total++;
+                }
+                virus[num[p]] = true;
 			}
+
 		}
+
+        if (flag) {
+            printf("web %d:", idx);
+            for (int i = 1; i <= n; i++) {
+                if (virus[i]) {
+                    printf(" %d", i);
+                }
+            }
+            printf("\n");
+            return true;
+        }
 
 		return false;
 	}
@@ -101,3 +158,5 @@ public:
 		return str_main;
 	}
 };
+
+
